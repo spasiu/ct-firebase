68282827e6288a291b9a894fe56343136850b561
Vedran Sisic <vedran.sisic@gmail.com>
2021-09-28 07:58:26 -0400

    Implement better sort

--- functions/utils/shuffleArray.js
+++ functions/utils/shuffleArray.js
@@ -1,12 +1,37 @@
-module.exports = function (arr) {
-  const shuffledArray = [...arr];
+const axios = require("axios");
+const functions = require("firebase-functions");
 
-  for (var i = shuffledArray.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = shuffledArray[i];
-    shuffledArray[i] = shuffledArray[j];
-    shuffledArray[j] = temp;
+const RAND_URL = "https://qrng.anu.edu.au/API/jsonI.php?type=uint16&length=";
+
+module.exports = async function (arr) {
+  let randomArrayData;
+  let randomArray;
+
+  // Get randomized array
+  try {
+    randomArrayData = await axios(`${RAND_URL}${arr.length}`);
+    randomArray = randomArrayData.data.data;
+  } catch (e) {
+    throw new functions.https.HttpsError(
+      "internal",
+      "Failed to get random array from source."
+    );
   }
+
+  // Set sort order for items
+  const shuffledArray = arr
+    .map((item, idx) => ({
+      ...item,
+      customSortOrder: randomArray[idx],
+    }))
+    .sort(function (a, b) {
+      return a.customSortOrder - b.customSortOrder;
+    });
+
+  // Remove custom property
+  shuffledArray.forEach(function (v) {
+    delete v.customSortOrder;
+  });
 
   return shuffledArray;
 };
