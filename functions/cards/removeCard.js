7c051e7635fda3475720bc883be829fe54818029
Nicholas Bakolias <bakoliasn@gmail.com>
2022-02-03 11:56:06 -0500

    proper error handling for removeCard

--- functions/cards/removeCard.js
+++ functions/cards/removeCard.js
@@ -13,58 +13,62 @@ const GET_USER_PAYSAFE_ID = gql`
     }
   }
 `;
-
-exports.removeCard = functions.https.onCall((data, context) => {
+const ERRORS = {
+  user_profile_missing: {
+    type: "user_profile_missing",
+    httpsArgs: [
+      "internal",
+      "User Paysafe profile does not exist",
+      { ct_error_code: "user_profile_missing" },
+    ],
+  },
+  user_card_not_deleted: {
+    type: "user_card_not_deleted",
+    httpsArgs: [
+      "internal",
+      "Could not remove card",
+      { ct_error_code: "user_card_not_deleted" },
+    ],
+  },
+};
+exports.removeCard = functions.https.onCall(async (data, context) => {
   authorize(context);
 
   const uid = context.auth.uid;
 
   const { cardId } = data;
-
-  /**
-   * Get user doc
-   */
-  return GraphQLClient.request(GET_USER_PAYSAFE_ID, { userId: uid }).then(
-    (response) => {
-      if (!response.Users_by_pk.paysafe_user_id) {
-        functions.logger.log(new Error(`User Paysafe profile does not exist, user: ${uid}`));
-        throw new functions.https.HttpsError(
-          "internal",
-          "User Paysafe profile does not exist",
-          { ct_error_code: "user_profile_missing" }
-        );
-      }
-        /**
-         * Remove card
-         */
-        const psRemoveCardOptions = {
-          url: `${
-            functions.config().env.paysafe.url
-          }/customervault/v1/profiles/${
-            response.Users_by_pk.paysafe_user_id
-          }/cards/${cardId}`,
-          method: "DELETE",
-          headers: {
-            Authorization: `Basic ${
-              functions.config().env.paysafe.serverToken
-            }`,
-            "Content-Type": "application/json",
-          },
-        };
-
-        return axios(psRemoveCardOptions)
-          .then((card) => {
-            return card.data;
-          })
-          .catch((e) => {
-            if(e.message === "User Paysafe profile does not exist") throw e
-            functions.logger.log(e, { status: e.response && e.response.status, data: e.response && e.response.data, userId: uid });
-            throw new functions.https.HttpsError(
-              "internal",
-              "Could not remove card",
-              { ct_error_code: "user_card_not_deleted" }
-            );
-          });
+  try {
+    /**
+     * Get user doc
+     */
+    const response = await GraphQLClient.request(GET_USER_PAYSAFE_ID, {
+      userId: uid,
+    });
+    if (!response.Users_by_pk.paysafe_user_id) {
+      throw new Error(ERRORS.user_profile_missing.type);
     }
-  );
+    /**
+     * Remove card
+     */
+    const psRemoveCardOptions = {
+      url: `${functions.config().env.paysafe.url}/customervault/v1/profiles/${
+        response.Users_by_pk.paysafe_user_id
+      }/cards/${cardId}`,
+      method: "DELETE",
+      headers: {
+        Authorization: `Basic ${functions.config().env.paysafe.serverToken}`,
+        "Content-Type": "application/json",
+      },
+    };
+    const card = await axios(psRemoveCardOptions);
+    return card.data;
+  } catch (e) {
+    const error = ERRORS[e.message] || ERRORS.user_card_not_deleted;
+    functions.logger.log(e, {
+      status: e.response && e.response.status,
+      data: e.response && e.response.data,
+      userId: uid,
+    });
+    throw new functions.https.HttpsError(...error.httpsArgs);
+  }
 });
