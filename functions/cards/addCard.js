8b7f5ebc61e2518e47594baa2e6d7cd613696277
Nicholas Bakolias <bakoliasn@gmail.com>
2022-02-03 08:50:33 -0500

    redo addCard errors

--- functions/cards/addCard.js
+++ functions/cards/addCard.js
@@ -36,6 +36,29 @@ const ADD_BILLING_ADDRESS = gql`
   }
 `;
 
+const ERRORS = {
+  user_profile_missing: {
+    type: "user_profile_missing",
+    httpsArgs: ["internal", "User Paysafe profile does not exist", { ct_error_code: "user_profile_missing" }]
+  },
+  avs_mismatch: {
+    type: "avs_mismatch",
+    httpsArgs: ["failed-precondition", "Failed avs verification", { ct_error_code: "avs_mismatch" }]
+  },
+  cvv_mismatch: {
+    type: "cvv_mismatch",
+    httpsArgs: ["failed-precondition", "Failed cvv verification", { ct_error_code: "cvv_mismatch" }]
+  },
+  could_not_verify_card: {
+    type: "could_not_verify_card",
+    httpsArgs: ["failed-precondition", "Could not verify card", { ct_error_code: "could_not_verify_card" }]
+  },
+  could_not_add_card: {
+    type: "could_not_add_card",
+    httpsArgs: ["failed-precondition", "Could not add card", { ct_error_code: "could_not_add_card" }]
+  }
+}
+
 exports.addCard = functions.https.onCall(async (data, context) => {
   authorize(context);
 
@@ -43,22 +66,17 @@ exports.addCard = functions.https.onCall(async (data, context) => {
 
   const { singleUseToken } = data;
 
-  /**
-   * Get user doc
-   */
-  const response = await GraphQLClient.request(GET_USER_PAYSAFE_ID, {
-    userId: uid,
-  });
-  if (!response.Users_by_pk.paysafe_user_id) {
-    functions.logger.log(new Error(`User Paysafe profile does not exist, user: ${uid}`));
-    throw new functions.https.HttpsError(
-      "internal",
-      "User Paysafe profile does not exist",
-      { ct_error_code: "user_profile_missing" }
-    );
-  }
-
   try {
+    /**
+     * Get user doc
+     */
+    const response = await GraphQLClient.request(GET_USER_PAYSAFE_ID, {
+      userId: uid,
+    });
+    if (!response.Users_by_pk.paysafe_user_id) {
+      throw new Error(ERRORS.user_profile_missing.type)
+    }
+
     const psVerifyCardOptions = {
       url: `${functions.config().env.paysafe.url}/cardpayments/v1/accounts/${
         functions.config().env.paysafe.accountId
@@ -70,7 +88,7 @@ exports.addCard = functions.https.onCall(async (data, context) => {
       },
       data: {
         card: {
-          paymentToken: singleUseToken,
+          paymentToken: singleUseToken, 
         },
         merchantRefNum: `${uid}-addCard-${Date.now()}`,
         storedCredential: {
@@ -79,6 +97,35 @@ exports.addCard = functions.https.onCall(async (data, context) => {
         },
       },
     };
+    /**
+     * Verify card token
+     */
+    const verify = await axios(psVerifyCardOptions);
+    const avs = verify.data.avsResponse;
+    const avsCvvError = (avs === "NO_MATCH" || avs === "NOT_PROCESSED" || avs === "UNKNOWN") || verify.data.cvvVerification !== "MATCH";
+    if (avsCvvError) {
+      const mismatch = verify.data.cvvVerification === "MATCH" ? "avs_mismatch" : "cvv_mismatch";
+      throw new Error(ERRORS[mismatch].type)
+    }
+    /**
+     * Add billing address to user Addresses
+     */
+    if(verify.data.billingDetails.country === "US" || verify.data.billingDetails.country === "CA"){
+      GraphQLClient.request(ADD_BILLING_ADDRESS, {
+        address: {
+          line1: verify.data.billingDetails.street,
+          line2: verify.data.billingDetails.street2,
+          postal_zip_code: verify.data.billingDetails.zip,
+          state_province_region: verify.data.billingDetails.state,
+          city: verify.data.billingDetails.city,
+          country: verify.data.billingDetails.country,
+          first_name: response.Users_by_pk.first_name,
+          last_name: response.Users_by_pk.last_name,
+          user_id: uid,
+        },
+      }).catch(e=>functions.logger.log(e, { status: e.response && e.response.status, data: e.response && e.response.data, userId: uid, cause: "Unable to add billing address to user" }));
+  }
+
     const psAddCardOptions = {
       url: `${functions.config().env.paysafe.url}/customervault/v1/profiles/${
         response.Users_by_pk.paysafe_user_id
@@ -93,46 +140,11 @@ exports.addCard = functions.https.onCall(async (data, context) => {
         accountId: functions.config().env.paysafe.accountId,
       },
     };
-    /**
-     * Verify card token
-     */
-    const verify = await axios(psVerifyCardOptions);
-    const avs = verify.data.avsResponse;
-    const avsCvvError = (avs === "NO_MATCH" || avs === "NOT_PROCESSED" || avs === "UNKNOWN") || verify.data.cvvVerification !== "MATCH";
-    if (avsCvvError) {
-      const mismatch = verify.data.cvvVerification === "MATCH" ? "avs_mismatch" : "cvv_mismatch";
-      functions.logger.log(new Error(`${mismatch} user: ${uid}`));
-      throw new functions.https.HttpsError(
-        "failed-precondition",
-        "Failed avs/cvv verification",
-        { ct_error_code: mismatch }
-      );
-    }
-
-    GraphQLClient.request(ADD_BILLING_ADDRESS, {
-      address: {
-        line1: verify.data.billingDetails.street,
-        line2: verify.data.billingDetails.street2,
-        postal_zip_code: verify.data.billingDetails.zip,
-        state_province_region: verify.data.billingDetails.state,
-        city: verify.data.billingDetails.city,
-        country: verify.data.billingDetails.country,
-        first_name: response.Users_by_pk.first_name,
-        last_name: response.Users_by_pk.last_name,
-        user_id: uid,
-      },
-    });
-
     const newCard = await axios(psAddCardOptions);
     return newCard.data;
   } catch (e) {
-    if(e.message === "Failed avs/cvv verification" || e.message === "User Paysafe profile does not exist") throw e
-    const verifyError = e.config && e.config.url.slice(e.config.url.length - 13, e.config.length) === "verifications";
+    const error = ERRORS[e.message] || (e.config && e.config.url.indexOf("verifications") > -1 ? ERRORS.could_not_verify_card : ERRORS.could_not_add_card);
     functions.logger.log(e, { status: e.response && e.response.status, data: e.response && e.response.data, userId: uid });
-    throw new functions.https.HttpsError(
-      "failed-precondition",
-      verifyError ? "Could not verify card" : "Could not add card",
-      { ct_error_code: verifyError ? "could_not_verify_card" : "could_not_add_card" }
-    );
+    throw new functions.https.HttpsError(...error.httpsArgs);
   }
 });
