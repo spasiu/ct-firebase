a5da3cdd061d535649f2f15063ef27677a9aa0d9
Jonathan Weiss <gneeri@gmail.com>
2022-05-16 10:39:54 -0400

    changes for user deletion

--- functions/user/deleteUser.js
+++ functions/user/deleteUser.js
@@ -1,7 +1,18 @@
 const functions = require("firebase-functions");
-
+const admin = require("firebase-admin");
 
 exports.deleteUser = functions.https.onCall(async (data) => {
-  const userId = data.id;
-  return userId
+  const uid = data.id;
+ 
+  /**
+   * delete user via firebase admin;
+   * this should trigger the onDeletUser func
+   * that will take care of the other accounts
+   */
+  try {
+    await admin.auth().deleteUser(uid);
+  } catch(error) {
+    functions.logger.error(error);
+  };
+
 });
